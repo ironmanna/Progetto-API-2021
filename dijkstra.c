db90b2182c3f74de43413f840fbfc9544bb5fb3a
Simone Mannarino <simo.manna00@gmail.com>
2021-08-03 19:44:52 +0200

    Risoluzione dei problemi su Dijkstra e copia su main

--- dijkstra.c
+++ dijkstra.c
@@ -12,62 +12,111 @@ struct NodoGrafo{
 typedef struct NodoGrafo Pezzo;
 typedef Pezzo *ListaPrioritaria;
 
-ListaPrioritaria inserimento(ListaPrioritaria Queue, int riga, int j, int valore){
+ListaPrioritaria inserimento(ListaPrioritaria Queue, int riga, int j, int valore, int peso){
     ListaPrioritaria p;
     if(Queue==NULL){
         Queue = malloc(sizeof(struct NodoGrafo));
         Queue->inizio = riga;
         Queue->fine = j;
-        Queue->costo = valore;
+        Queue->costo = valore + peso;
         Queue->next = NULL;
         return Queue;
     }
-    if (valore < Queue->costo) {
+    if (valore + peso < Queue->costo) {
         p = malloc (sizeof(struct NodoGrafo));
-        p->costo = valore;
         p->inizio = riga;
         p->fine = j;
+        p->costo = valore + peso;
         p->next = Queue;
         return p;
     }    
-    Queue->next = inserimento(Queue->next,riga, j, valore);
+    Queue->next = inserimento(Queue->next,riga, j, valore, peso);
     return Queue;
 }
 
 void stampaLista(ListaPrioritaria Queue){
+    ListaPrioritaria Trash;
     while (Queue!=NULL){
         printf("Inizio: %d, Fine: %d, Costo: %d\n",Queue->inizio, Queue->fine, Queue->costo);
+        Trash = Queue;
         Queue = Queue->next;
+        free(Trash);
     }
 }
 
+ListaPrioritaria successivo(ListaPrioritaria Queue,int d, int Tabella[d][d], int *riga){
+    ListaPrioritaria Trash = NULL;
+    while(Queue!=NULL){
+        Trash = Queue;
+        if(Tabella[Queue->fine][0]==(d+1)){
+            Tabella[Queue->fine][0] = Queue->fine;
+            Tabella[Queue->fine][1] = Queue->inizio;
+            Tabella[Queue->fine][2] = Queue->costo;
+            *riga = Queue->fine;
+            Queue = Queue->next;
+            printf("Liberata da 1 -> Inizio: %d, Fine: %d, Costo: %d\n",Trash->inizio, Trash->fine, Trash->costo);
+            free(Trash);
+            return Queue;
+        }
+        Queue = Queue->next;
+        printf("Liberata da 2 -> Inizio: %d, Fine: %d, Costo: %d\n",Trash->inizio, Trash->fine, Trash->costo);
+        free(Trash);
+    }
+    return Queue;
+}
+
 int main(){
-    int Grafo[3][3];
+    int d = 3;
+    int Grafo[d][d];
     srand(time(NULL));
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
+    /*for(int i=0; i<d; i++){
+        for(int j=0; j<d; j++){
             Grafo[i][j] = rand()%100;
         }
     }
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
+    for(int i=0; i<d; i++){
+        for(int j=0; j<d; j++){
             printf("%d ",Grafo[i][j]);
         }
         printf("\n");
+    }*/
+    Grafo[0][0] = 3;
+    Grafo[0][1] = 1;
+    Grafo[0][2] = 8;
+    Grafo[1][0] = 0;
+    Grafo[1][1] = 0;
+    Grafo[1][2] = 5;
+    Grafo[2][0] = 0;
+    Grafo[2][1] = 9;
+    Grafo[2][2] = 0;
+    int riga = 0,valore = 0, peso = 0, punteggio = 0;
+    ListaPrioritaria Queue = NULL;
+    int TabellaVisite[d][3];
+    TabellaVisite[0][0] = 0;
+    TabellaVisite[0][1] = 0;
+    TabellaVisite[0][2] = 0;
+    for (int i = 1; i < d; i++)
+    {
+        TabellaVisite[i][0] = (d+1);
     }
-    int riga = 0,valore = 0;
-    ListaPrioritaria Queue;
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            if(riga!=j){
+    for(int i=0; i<d; i++){
+        for(int j=0; j<d; j++){
+            if(riga!=j && Grafo[riga][j]!=0){
                 valore = Grafo[riga][j];
                 printf("%d ",valore);
-                Queue = inserimento (Queue, riga, j, valore);
+                Queue = inserimento (Queue, riga, j, valore, peso);
             }
         }
-        riga++;
+        printf("\n");
+        if(Queue!=NULL) Queue = successivo(Queue,d,TabellaVisite,&riga);
+        else break;
+        peso = TabellaVisite[riga][2];
     }
     printf("\n");
-    stampaLista(Queue);
+    for (int i = 0; i < d; i++)
+    {
+        punteggio += TabellaVisite [i][2];
+    }
+    printf("Punteggio: %d\n",punteggio);
     return 0;
 }
--- main.c
+++ main.c
@@ -22,9 +22,10 @@ AggiungiGrafo(FILE *fin)
 TopK()
     Stampa i valori minimi dalla lista
 
- L'idea è di usare l'algoritmo di Dijkstra con una struttura a Heap di Fibonacci per ottenere le prestazioni 
- temporali e spaziali migliori
+ L'idea è di usare l'algoritmo di Dijkstra con una lista per organizzare le visite successive 
 */
+
+#define BUFFER 10000
 
 struct Nodo{
     int valore;
@@ -44,135 +45,165 @@ struct NodoGrafo{
 typedef struct NodoGrafo Pezzo;
 typedef Pezzo *ListaPrioritaria;
 
-Lista inserisci_ordine_ricorsivo1 (Lista l, int valore);
-
-ListaPrioritaria inserisci_ordine_ricorsivo2 (ListaPrioritaria l, int costo, int inizio, int peso, int fine);
-
-int AggiungiGrafo(FILE *fin, int d);
-
-void TopK (Lista l, int k);
-
-int SceltaSuccessivo(ListaPrioritaria Queue, int d, int TabellaVisite[3][d]);
-
-int main() {
-    int d, k, punteggio, count=0;
-    char lettura[16];
-    FILE *fin;
-    Lista valori_finali = NULL;
-    fin = fopen ("input.txt", "r");
-    if (fin == NULL) {
-        printf("La lettura non é andata a buon fine"); 
-        exit(1);
+ListaPrioritaria inserimento(ListaPrioritaria Queue, int riga, int j, int valore, int peso){
+    ListaPrioritaria p;
+    if(Queue==NULL){
+        Queue = malloc(sizeof(struct NodoGrafo));
+        Queue->inizio = riga;
+        Queue->fine = j;
+        Queue->costo = valore + peso;
+        Queue->next = NULL;
+        return Queue;
     }
-    if (fscanf(fin, "%d ,", &d) == 0) printf("Errore nella prima lettura\n");
-    if (fscanf(fin, "%d\n", &k) == 0) printf("Errore nella seconda lettura\n");
-    while(!feof(fin)){
-        printf("%d\n",count);
-        if (fgets(lettura,16,fin) == NULL) printf("Errore nella fgets %s\n",lettura);
-        if (strcmp(lettura,"AggiungiGrafo\n")==0){ //Che cazzo ho scritto? -> Segmentation Fault 11 qui
-            printf("Sono qui 2\n"); 
-            punteggio = AggiungiGrafo(fin, d);
-            printf("Sono qui 3 %d\n",punteggio);
-            inserisci_ordine_ricorsivo1(valori_finali, punteggio);
-        }
-        if (strcmp(lettura,"TopK")==0) TopK(valori_finali, k );
-        count++;
-    }
-    fclose(fin);
-    return 0;
-}
-
-void TopK (Lista l, int k){
-    for (int i = 0; i<k; i++){
-        printf("%d ", l->valore);
-        l = l->next;
-    }
-}
-
-Lista inserisci_ordine_ricorsivo1 (Lista l, int valore){
-    Lista p;
-    if (l == NULL){
-        p = malloc (sizeof(struct Nodo));
-        p->valore = valore;
-        p->next = NULL;
-        return p;
-    }
-    if (valore < l->valore) {
-        p = malloc (sizeof(struct Nodo));
-        p->valore = valore;
-        p->next = l;
+    if (valore + peso < Queue->costo) {
+        p = malloc (sizeof(struct NodoGrafo));
+        p->inizio = riga;
+        p->fine = j;
+        p->costo = valore + peso;
+        p->next = Queue;
         return p;
     }    
-    l->next = inserisci_ordine_ricorsivo1(l->next,valore);
-    return l;
+    Queue->next = inserimento(Queue->next,riga, j, valore, peso);
+    return Queue;
 }
 
-ListaPrioritaria inserisci_ordine_ricorsivo2 (ListaPrioritaria l, int costo, int inizio, int peso, int fine){
-    ListaPrioritaria p;
-    if (l == NULL){
-        p = malloc (sizeof(struct NodoGrafo));
-        p->costo = costo + peso;
-        p->inizio = inizio;
-        p->fine = fine;
-        p->next = NULL;
-        return p;
+void stampaLista(ListaPrioritaria Queue){
+    ListaPrioritaria Trash;
+    while (Queue!=NULL){
+        printf("Inizio: %d, Fine: %d, Costo: %d\n",Queue->inizio, Queue->fine, Queue->costo);
+        Trash = Queue;
+        Queue = Queue->next;
+        free(Trash);
     }
-    if ((costo + peso) < l->costo) {
-        p = malloc (sizeof(struct NodoGrafo));
-        p->costo = costo + peso;
-        p->inizio = inizio;
-        p->fine = fine;
-        p->next = l;
-        return p;
-    }
-
-    l->next = inserisci_ordine_ricorsivo2(l->next,costo,inizio,peso,fine);
-    return l;
 }
 
-int SceltaSuccessivo(ListaPrioritaria Queue,int d, int TabellaVisite[3][d]){
-    if(Queue == NULL){
-        return 0;
-    }
-    TabellaVisite[0][Queue->fine] = Queue->fine;
-    TabellaVisite[1][Queue->fine] = Queue->inizio;
-    TabellaVisite[2][Queue->fine] = Queue->costo;
-    int selezione = Queue->fine;
-    ListaPrioritaria Trash = Queue;
-    Queue = Queue->next;
-    free(Trash);
-    return selezione;
-}
-
-
-int AggiungiGrafo(FILE *fin, int d) {
-    int matrice [d][d];
-    for (int i = 0; i<d; i++){
-        if(fscanf(fin,"%d", &matrice[i][0])==0) printf("Errore nella lettura della prima cella\n");
-        for (int j = 1; j<d; j++){
-            if(fscanf(fin,", %d", &matrice[i][j])==0) printf("Errore nella lettura delle celle\n");; //attenzione alle virgole
+ListaPrioritaria successivo(ListaPrioritaria Queue,int d, int Tabella[d][d], int *riga){
+    ListaPrioritaria Trash = NULL;
+    while(Queue!=NULL){
+        Trash = Queue;
+        if(Tabella[Queue->fine][0]==(d+1)){
+            Tabella[Queue->fine][0] = Queue->fine;
+            Tabella[Queue->fine][1] = Queue->inizio;
+            Tabella[Queue->fine][2] = Queue->costo;
+            *riga = Queue->fine;
+            Queue = Queue->next;
+            printf("Liberata da 1 -> Inizio: %d, Fine: %d, Costo: %d\n",Trash->inizio, Trash->fine, Trash->costo);
+            free(Trash);
+            return Queue;
         }
+        Queue = Queue->next;
+        printf("Liberata da 2 -> Inizio: %d, Fine: %d, Costo: %d\n",Trash->inizio, Trash->fine, Trash->costo);
+        free(Trash);
     }
-    int TabellaVisite[3][d-1];
-    TabellaVisite[0][0] = 0;
-    TabellaVisite[1][0] = 0;
-    TabellaVisite[2][0] = 0;
+    return Queue;
+}
+
+int Dijkstra(int d, int matrice[d][d]){
+    int riga = 0,valore = 0, peso = 0, punteggio = 0;
     ListaPrioritaria Queue = NULL;
-    int selezione = 0;
-    int peso = 0;
-    for (int i = 0; i < d; i++)
+    int TabellaVisite[d][3];
+    TabellaVisite[0][0] = 0;
+    TabellaVisite[0][1] = 0;
+    TabellaVisite[0][2] = 0;
+    for (int i = 1; i < d; i++)
     {
-        for (int j = 0; j<d; j++){
-            if (matrice[selezione][j] != 0 && selezione != j){
-                Queue = inserisci_ordine_ricorsivo2(Queue,matrice[selezione][j],selezione,peso,j);
+        TabellaVisite[i][0] = (d+1);
+    }
+    for(int i=0; i<d; i++){
+        for(int j=0; j<d; j++){
+            if(riga!=j && matrice[riga][j]!=0){
+                valore = matrice[riga][j];
+                printf("%d ",valore);
+                Queue = inserimento (Queue, riga, j, valore, peso);
             }
         }
-        selezione = SceltaSuccessivo(Queue,d,TabellaVisite);
-        if (i>0 && selezione == 0) break;
+        printf("\n");
+        if(Queue!=NULL) Queue = successivo(Queue,d,TabellaVisite,&riga);
+        else break;
+        peso = TabellaVisite[riga][2];
     }
-    int somma = 0;
-    for(int i = 0; i<d; i++){
-        somma+=TabellaVisite[2][i];
+    printf("\n");
+    for (int i = 0; i < d; i++)
+    {
+        punteggio += TabellaVisite [i][2];
     }
-    return somma;
+    return punteggio;
+}
+
+int AggiungiGrafo(FILE *fin, int d){
+    int matrice [d][d];
+    for (int i = 0; i < d; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            matrice[i][j] = 0;
+        }
+    }
+    int colonna=0;
+    int caratteriLetti;
+    char riga[BUFFER];
+    for (int i = 0; i < d; i++)
+    {
+        if(fgets(riga, BUFFER, fin) == NULL) printf("Problema 1\n");
+        caratteriLetti = strlen(riga);
+        if(riga[caratteriLetti-1] == '\n') riga[caratteriLetti-1]='\0';
+        for(int j=0;j<caratteriLetti;j++){
+           if (riga[j] != 44 && riga[j] != 0){
+               if(matrice[i][colonna]==0) matrice[i][colonna]=riga[j]-48;
+               else matrice[i][colonna] = matrice[i][colonna]*10 + riga[j]-48;
+           }
+           if (riga[j]==44) colonna++;
+        }
+        colonna = 0;
+    }
+    return Dijkstra(d,matrice);    
+}
+
+Lista accodamento (Lista best, int punteggio){
+    Lista p;
+    if(best==NULL){
+        best = malloc(sizeof(struct Nodo));
+        best->valore = punteggio;
+        best->next = NULL;
+        return best;
+    }
+    if (punteggio < best->valore) {
+        p = malloc (sizeof(struct Nodo));
+        p->valore = punteggio;
+        p->next = best;
+        return p;
+    }    
+    best->next = accodamento(best->next,punteggio);
+    return best;
+}
+
+void TopK(Lista best, int k){
+    Lista Trash = NULL;
+    while(best!=NULL || k!=0){
+        printf("%d ",best->valore);
+        k--;
+        Trash = best;
+        best = best->next;
+        free(Trash);
+    }
+}
+
+int main() {
+    FILE *fin;
+    Lista classifica = NULL;
+    fin = fopen("input.txt","r");
+    int d,k, caratteriLetti;
+    char lettura[BUFFER];
+    if (fscanf(fin, "%d ,", &d) == 0) printf("Errore nella prima lettura\n");
+    if (fscanf(fin, "%d\n", &k) == 0) printf("Errore nella seconda lettura\n");
+    while(fgets(lettura, BUFFER, fin) != NULL) 
+    {
+        caratteriLetti = strlen(lettura);
+        if(lettura[caratteriLetti-1] == '\n') lettura[caratteriLetti-1]='\0';
+        if (!strcmp(lettura,"AggiungiGrafo")) classifica = accodamento(classifica, AggiungiGrafo(fin,d));
+        if (!strcmp(lettura,"TopK")) TopK(classifica, k);
+
+    } 
+    fclose (fin);
+    return 0;
 }
