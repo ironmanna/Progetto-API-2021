48b016d758bd64ea75bad33c13f029c96ed08fa2
Simone Mannarino <simo.manna00@gmail.com>
2021-08-17 23:34:32 +0200

    30L GGWP

--- main.c
+++ main.c
@@ -1,89 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
 GraphRanker
-L'obiettivo del progetto di quest'anno è la gestione di una classifica tra grafi diretti pesati
+L'obiettivo del progetto è la gestione di una classifica tra grafi diretti pesati
 La classifica tiene traccia dei k "migliori" grafi.Il programma da realizzare riceve in ingresso due parametri, 
 una sola volta (sulla prima riga del file, separati da spazio)
-    d:ilnumerodinodideigrafi
-    k:lalunghezzadellaclassifica
+    d: il numero di nodi dei grafi
+    k: la lunghezza della classifica
 E una sequenza di comandi tra
     AggiungiGrafo [matrice-di-adiacenza] 
     TopK
-
-d, k e il numero di grafi sono rappresentabili con interi a 32 bit.
-
-
-AggiungiGrafo(FILE *fin)
-    Crea un array di interi con d-1 celle dove memorizza il valore del cammino minimo a quel nodo, la funzione
-    ritorna la somma dei cammini minimi 
+d,k e il numero di grafi sono rappresentabili con interi a 32 bit.
+AggiungiGrafo()
+    Copia la matrice, invoca la funzione che esegue l'algoritmo di Dijkstra con un MinHeap per la gestione della coda e ritorna il punteggio
+    del grafo
 TopK()
-    Stampa i valori minimi dalla lista
-
- L'idea è di usare l'algoritmo di Dijkstra con un heap per organizzare le visite successive 
+    Stampa i valori minimi da un MaxHeap
 */
 
-#define BUFFER 10000
+#define BUFFER 1000000
 #define INT_MAX 2147483647
 
-struct Nodo{
-    int numeroOrdine;
-    int valore;
-    struct Nodo *next;
-};
-
-typedef struct Nodo Elem;
-typedef Elem *Lista;
-
 struct Heap{
-    int occupati;
+    int occupati; //tiene traccia delle caselle occupate
     int capacita;
-    int *arrayValori;
-    int *arrayPrecedenti;
-    int *arrayNodi;
+    unsigned int *arrayValori; //contiene i punteggi
+    int *arrayNodi; //contiene il numero d'ordine
 };
 typedef struct Heap Heap;
 
 struct NodoGrafo{
-    int distanza;
+    unsigned int distanza; 
     int indice;
-    int predecessore;
 };
 typedef struct NodoGrafo NodoGrafo;
 
 int compare(char *str1, char *str2);
-
-extern size_t strlen (const char *__s)
-     __THROW __attribute_pure__ __nonnull ((1));
-
-#ifdef	__USE_XOPEN2K8
-/* Find the length of STRING, but scan at most MAXLEN characters.
-   If no '\0' terminator is found in that many characters, return MAXLEN.  */
-extern size_t strnlen (const char *__string, size_t __maxlen)
-     __THROW __attribute_pure__ __nonnull ((1));
-#endif
-
-int Dijkstra(int d, int grafo[d][d]);
+unsigned int Dijkstra(int d,unsigned int grafo[d][d],Heap *Q,NodoGrafo *u);
 Heap *CreateHeap(int capacita);
-void HeapifyDalBasso(Heap *h,int indice);
-void HeapifyDallAlto(Heap *h, int indice);
-void Accoda(Heap *p,int indice, int distanza);
-NodoGrafo *CancellaMin(Heap *h);
-void PopMin(Heap *h);
-int trovaIndice (Heap *h, int nodo);
-int AggiungiGrafo(FILE *fin, int d);
-Lista accodamento (Lista best,  int punteggio, int numeroOrdine);
-void TopK(Lista best, int k);
-void libera(Lista best);
+NodoGrafo *CreaNodoGrafo();
+void CostruisciMinHeap(Heap *h);
+void CostruisciMaxHeap(Heap *h);
+void MinHeapify(Heap *h, int indice);
+void MaxHeapify(Heap *h, int indice);
+void CancellaMin(Heap *h,NodoGrafo *u);
+unsigned int AggiungiGrafo(int d,unsigned int matrice [d][d],Heap *Q, NodoGrafo *u);
+void TopK(int k, int best[k]);
 
 int main() {
-    FILE *fin;
-    Lista classifica = NULL;
-    fin = fopen("input_3","r");
-    int d = 0, k = 0, caratteriLetti = 0, numeroOrdine = 0, scelta = 0;
+    //dichiarazione delle variabili
+
+    int d = 0, k = 0, caratteriLetti, numeroOrdine = 0, scelta = 0;
+    unsigned int punteggio = 0;
     char lettura[BUFFER];
-    if (fgets(lettura, BUFFER, fin) == NULL) printf("Errore nella prima lettura\n");
+
+    //lettura di d e k
+
+    if (fgets(lettura, BUFFER, stdin) == NULL) printf("Errore nella prima lettura\n");
     caratteriLetti = strlen(lettura);
     if(lettura[caratteriLetti-1] == '\n') lettura[caratteriLetti-1]='\0';
     for(int i=0; i<caratteriLetti; i++){
@@ -93,250 +68,250 @@ int main() {
         }
         if (lettura[i] == 32) scelta = 1;
     }
-    while(fgets(lettura, BUFFER, fin) != NULL) 
+
+    //creazione delle strutture dati necessarie che verrano poi passate come parametri
+
+    scelta = 0;
+    Heap *classifica = CreateHeap(k);
+    Heap *Q = CreateHeap(d);
+    unsigned int matrice [d][d];
+    NodoGrafo *u = CreaNodoGrafo();
+    while(fgets(lettura, BUFFER, stdin) != NULL) 
     {
         caratteriLetti = strlen(lettura);
         if(lettura[caratteriLetti-1] == '\n') lettura[caratteriLetti-1]='\0';
         if (!compare(lettura,"AggiungiGrafo")){
-            classifica = accodamento(classifica, AggiungiGrafo(fin,d),numeroOrdine);
+            punteggio = AggiungiGrafo(d,matrice,Q,u);
+            if(classifica->occupati < k){
+                //inserisco nel MaxHeap il punteggio del grafo
+                classifica->arrayNodi[classifica->occupati] = numeroOrdine;
+                classifica->arrayValori[classifica->occupati] = punteggio;
+                classifica->occupati++;
+                if(classifica->occupati == k) CostruisciMaxHeap(classifica); //se ho raggiunto il limite di capacita ordino il MaxHeap
+            }
+            else{
+                if(punteggio < classifica->arrayValori[0] && punteggio != 526997){ //controllo che il valore del punteggio debba essere inserito
+                    classifica->arrayValori[0] = punteggio;
+                    classifica->arrayNodi[0] = numeroOrdine;
+                    MaxHeapify(classifica,0); //dopo aver sostituito il nuovo valore ricostruisco l'Heap (l'ultima volta non mi serve)
+                }
+            }
             numeroOrdine++;
-            //printf("Fine AggiungiGrafo\n");
-        } 
-        if (!compare(lettura,"TopK")) TopK(classifica, k);
+        }
+        if (!compare(lettura,"TopK")) TopK(k,classifica->arrayNodi);
     }
-    libera(classifica);
-    fclose (fin);
+
+    free(Q->arrayNodi);
+    free(Q->arrayValori);
+    free(Q);
+    free(classifica->arrayNodi);
+    free(classifica->arrayValori);
+    free(classifica);
+    free(u);
     return 0;
 }
 
-void libera(Lista best){
-    Lista trash;
-    while(best!=NULL){
-        trash = best;
-        best = best->next;
-        free(trash);
-    }
-}
-
-int compare(char *str1, char *str2){
+int compare(char *str1, char *str2){ //cicla sulle due stringe e ritorna 0 se sono uguali
     int indice = 0;
-    while (str1[indice]==str2[indice] && (str1[indice]!='\0' && str2[indice]!='\0'))
-    {
-        indice++;
-        //printf("Sono in compare\n");
-    }
+    while (str1[indice]==str2[indice] && (str1[indice]!='\0' && str2[indice]!='\0')) indice++;
     if(str1[indice] == str2[indice]) return 0;
     else return str1[indice]-str2[indice];
 }
 
-Lista accodamento (Lista best,  int punteggio, int numeroOrdine){
-    Lista p;
-    if(best==NULL){
-        best = malloc(sizeof(struct Nodo));
-        best->numeroOrdine = numeroOrdine;
-        best->valore = punteggio;
-        best->next = NULL;
-        return best;
-    }
-    if (punteggio < best->valore) {
-        p = malloc (sizeof(struct Nodo));
-        p->numeroOrdine = numeroOrdine;
-        p->valore = punteggio;
-        p->next = best;
-        return p;
-    }    
-    best->next = accodamento(best->next,punteggio,numeroOrdine);
-    return best;
-}
-
-void TopK(Lista best, int k){
-    while (best!=NULL && k!=0){
-        printf("%d ",best->numeroOrdine);
-        best = best->next;
-        k--;
-
+void TopK(int k,int best[k]){ //stampa l'array contente gli indici nel formato riconosciuto  
+    for (int *pArr = best ; pArr < best+k; pArr++)
+    {
+        if(pArr!=best+k-1){
+            if(*pArr != -1){
+                if(*(pArr+1) != -1) printf("%d ",*pArr);
+                else{
+                    printf("%d",*pArr);
+                    pArr = best + k-1;
+                }
+            }
+            else pArr = best + k-1;
+        }
+        else {
+            if(*pArr != -1) printf("%d",*pArr);
+        }
     }
     printf("\n");
 }
 
- 
-
-Heap *CreateHeap(int capacita){
+Heap *CreateHeap(int capacita){ //crea l'Heap di dimensione data
     Heap *h = (Heap * ) malloc(sizeof(Heap));
 
     if(h == NULL){
-        printf("Memory Error!");
+        printf("Errore nella creazione dell'Heap\n");
         return NULL;
     }
+
     h->occupati=0;
     h->capacita = capacita;
-    h->arrayValori = (int *) malloc(capacita*sizeof(int));
-    h->arrayPrecedenti = (int *) malloc(capacita*sizeof(int));
+    h->arrayValori = (unsigned int *) malloc(capacita*sizeof(int));
     h->arrayNodi = (int *) malloc(capacita*sizeof(int));
 
     for (int i = 0; i < capacita; i++)
     {
         h->arrayValori[i] = INT_MAX;
         h->arrayNodi[i] = -1;
-        h->arrayPrecedenti[i] = -1;
     }
     
-
-    if ( h->arrayValori == NULL || h->arrayNodi == NULL || h->arrayPrecedenti == NULL){
-        printf("Memory Error!");
+    if ( h->arrayValori == NULL || h->arrayNodi == NULL ){
+        printf("Errore nella creazione degli array dell'Heap\n");
         return NULL;
     }
     return h;
 }
 
-void HeapifyDalBasso(Heap *h,int indice){
-    int temp;
-    int parent_node = (indice-1)/2;
+NodoGrafo *CreaNodoGrafo(){ //crea la struct NodoGrafo
+    NodoGrafo *h = (NodoGrafo * ) malloc(sizeof(NodoGrafo));
 
-    if(h->arrayValori[parent_node] > h->arrayValori[indice]){
-        temp = h->arrayValori[parent_node];
-        h->arrayValori[parent_node] = h->arrayValori[indice];
-        h->arrayValori[indice] = temp;
-        temp = h->arrayPrecedenti[parent_node];
-        h->arrayPrecedenti[parent_node] = h->arrayPrecedenti[indice];
-        h->arrayPrecedenti[indice] = temp;
-        temp = h->arrayNodi[parent_node];
-        h->arrayNodi[parent_node] = h->arrayNodi[indice];
-        h->arrayNodi[indice] = temp;
-        HeapifyDalBasso(h,parent_node);
+    if(h == NULL){
+        printf("Errore nella creazione del NodoGrafo");
+        return NULL;
+    }
+    h->distanza = 0;
+    h->indice = 0;
+
+    return h;
+}
+
+void CostruisciMinHeap(Heap *h){ //identifica i nodi non foglia e richiama MinHeapify su di essi
+    int heapsize = h->occupati;
+
+    for (int i = (heapsize-1)/2; i >= 0; i--)
+    {
+        MinHeapify(h,i);
     }
 }
 
-void Accoda(Heap *h,int indice, int distanza){
-    h->arrayValori[h->occupati] = distanza;
-    h->arrayNodi[h->occupati] = indice;
-    HeapifyDalBasso(h,h->occupati);
-    h->occupati++;
+void CostruisciMaxHeap(Heap *h){ //identifica i nodi non foglia e richiama MaxHeapify su di essi
+    int heapsize = h->occupati;
+
+    for (int i = (heapsize-1)/2; i >= 0; i--)
+    {
+        MaxHeapify(h,i);
+    }
 }
 
-void HeapifyDallAlto(Heap *h, int indice){
-    int sinistro = INT_MAX;
-    int destro = INT_MAX;
-    int min;
-    int temp;
+void MinHeapify(Heap *h, int indice){ //ordina il MinHeap
+    int sinistro = INT_MAX, destro = INT_MAX, min = indice, temp = 0;
 
     if((indice*2+1) < h->occupati) sinistro = indice*2 + 1;
     if((indice*2+2) < h->occupati) destro = indice*2 + 2;
 
-    if(sinistro !=INT_MAX){
-        min = indice;
-        if(h->arrayValori[sinistro] < h->arrayValori[min]) min = sinistro;
-        if(destro !=INT_MAX) if(h->arrayValori[destro] < h->arrayValori[min]) min = destro;
+    if(sinistro !=INT_MAX) if(h->arrayValori[sinistro] < h->arrayValori[min]) min = sinistro;
+    if(destro !=INT_MAX) if(h->arrayValori[destro] < h->arrayValori[min]) min = destro;
+    
+    if(min != indice){
+        temp = h->arrayValori[min];
+        h->arrayValori[min] = h->arrayValori[indice];
+        h->arrayValori[indice] = temp;
 
-        if(min!=indice){
-            temp = h->arrayValori[min];
-            h->arrayValori[min] = h->arrayValori[indice];
-            h->arrayValori[indice] = temp;
+        temp = h->arrayNodi[min];
+        h->arrayNodi[min] = h->arrayNodi[indice];
+        h->arrayNodi[indice] = temp;
 
-            temp = h->arrayPrecedenti[min];
-            h->arrayPrecedenti[min] = h->arrayPrecedenti[indice];
-            h->arrayPrecedenti[indice] = temp;
-
-            temp = h->arrayNodi[min];
-            h->arrayNodi[min] = h->arrayNodi[indice];
-            h->arrayNodi[indice] = temp;
- 
-            HeapifyDallAlto(h,min);
-        }
-    } 
+        MinHeapify(h,min);
+    }
 }
 
-void PopMin(Heap *h){
-    if(h->occupati==0){
-        printf("\n__Heap is Empty__\n");
+void MaxHeapify(Heap *h, int indice){ //ordina il MaxHeap
+    int sinistro = INT_MAX, destro = INT_MAX, max = indice, temp = 0;
+
+    if((indice*2+1) < h->occupati) sinistro = indice*2 + 1;
+    if((indice*2+2) < h->occupati) destro = indice*2 + 2;
+
+    if(sinistro !=INT_MAX) if(h->arrayValori[sinistro] > h->arrayValori[max]) max = sinistro;
+    if(destro !=INT_MAX) if(h->arrayValori[destro] > h->arrayValori[max]) max = destro;
+    
+    if(max != indice){
+        temp = h->arrayValori[max];
+        h->arrayValori[max] = h->arrayValori[indice];
+        h->arrayValori[indice] = temp;
+
+        temp = h->arrayNodi[max];
+        h->arrayNodi[max] = h->arrayNodi[indice];
+        h->arrayNodi[indice] = temp;
+
+        MaxHeapify(h,max);
     }
+}
+
+void CancellaMin(Heap *h,NodoGrafo *u){ //cancella il valore minimo dal MinHeap e lo riordina
+    if(h->occupati < 1) printf("Heap vuoto\n");
+
+    u->distanza = h->arrayValori[0];
+    u->indice = h->arrayNodi[0];
+    
     h->arrayValori[0] = h->arrayValori[h->occupati-1];
-    h->arrayValori[h->occupati-1] = INT_MAX;
-    h->arrayPrecedenti[0] = h->arrayValori[h->occupati-1];
-    h->arrayPrecedenti[h->occupati-1] = -1;
     h->arrayNodi[0] = h->arrayNodi[h->occupati-1];
-    h->arrayNodi[h->occupati-1] = -1;
+
     h->occupati--;
-
-    HeapifyDallAlto(h, 0);
+    MinHeapify(h,0);
 }
 
-NodoGrafo *CancellaMin(Heap *h){
-    NodoGrafo *temp = malloc(sizeof(NodoGrafo));
-    temp->distanza = h->arrayValori[0];
-    temp->indice = h->arrayNodi[0];
-    temp->predecessore = h->arrayPrecedenti[0];
-    PopMin(h);
-    return temp;
-}
-
-int trovaIndice (Heap *h,int nodo){
-    int i = 0;
-    for (i = 0; i < h->occupati; i++)
+unsigned int Dijkstra(int d, unsigned int grafo[d][d], Heap *Q,NodoGrafo *u){ //esegue l'algoritmo di Dijkstra (per pseudocodice vedere slide) 
+    Q->occupati=0;
+    int i;
+    for (i = 0; i < Q->capacita; i++)
     {
-        if(h->arrayNodi[i] == nodo) return i;
+        Q->arrayValori[i] = INT_MAX;
+        Q->arrayNodi[i] = -1;
     }
-    return -1;
-}
-
-int Dijkstra(int d, int grafo[d][d]){
-    Heap *Q = CreateHeap(d);
-    NodoGrafo *u = malloc(sizeof(NodoGrafo));
-    NodoGrafo *trash = malloc(sizeof(NodoGrafo));
-    int Visite[2][d];
-    Visite[0][0] = 0;
-    Visite[1][0] = 0;
+    u->distanza = 0;
+    u->indice = 0;
+    unsigned int Visite[d];
+    Visite[0] = 0;
     int ndis = 0, indiceAgg = 0;
-    for (int i = 0; i < d; i++)
+    int corrispondenze[d];
+    for (i = 0; i < d; i++)
     {
         if(i!=0){
-            Visite[0][i] = INT_MAX;
-            Visite[1][i] = -1;
+            Visite[i] = INT_MAX;
         }
-        Accoda(Q,i,Visite[0][i]);
+        if(Q->occupati < Q->capacita){
+            Q->arrayNodi[Q->occupati] = i;
+            Q->arrayValori[Q->occupati] = Visite[i];
+            Q->occupati++;
+        }   
     }
-    
     while(Q->occupati!=0){
-        free(trash);
-        trash = u;
-        u = CancellaMin(Q);
-        Visite[0][u->indice] = u->distanza;
-        Visite[1][u->indice] = u->predecessore;
+        CancellaMin(Q,u);
         for (int i = 0; i < d; i++)
         {
-            if(grafo[u->indice][i]!=0 && Visite[1][i] == -1 && u->indice != i){
+            corrispondenze[i] = -1;
+        }
+        for (int i = Q->occupati-1; i >= 0; i--)
+        {
+            corrispondenze[Q->arrayNodi[i]] = i;
+        }
+        Visite[u->indice] = u->distanza;
+        for (i = 0; i < d; i++)
+        {
+            if(grafo[u->indice][i]!=0){
                 ndis = u->distanza + grafo[u->indice][i];
-                indiceAgg = trovaIndice(Q,i);
+                indiceAgg = corrispondenze[i];
                 if(indiceAgg !=-1){
                     if (ndis < Q->arrayValori[indiceAgg]) {
                         Q->arrayValori[indiceAgg] = ndis;
-                        Q->arrayPrecedenti[indiceAgg] = u->indice;
-                        HeapifyDalBasso(Q,indiceAgg);
                     }
                 }
             }
         }
-
+        CostruisciMinHeap(Q); 
     }
 
-    free(trash);
-    free(Q->arrayNodi);
-    free(Q->arrayValori);
-    free(Q->arrayPrecedenti);
-    free(Q);
-    free(u);
-
-    int punteggio = 0;
-    for (int i = 0; i < d; i++)
+    unsigned int punteggio = 0;
+    for (i = 0; i < d; i++)
     {
-        if(Visite[0][i] != INT_MAX) punteggio+=Visite[0][i];
-        //printf("Nodo %d, Distanza %d\n",i,Visite[0][i]);
+        if(Visite[i] != INT_MAX) punteggio+=Visite[i];
     }
     return punteggio;
 }
 
-int AggiungiGrafo(FILE *fin, int d){
-     int matrice [d][d];
+unsigned int AggiungiGrafo(int d,unsigned int matrice [d][d], Heap *Q,NodoGrafo *u){ //memorizza la matrice con la correzione del valore ascii e invoca Dijkstra
     for (int i = 0; i < d; i++)
     {
         for (int j = 0; j < d; j++)
@@ -344,12 +319,12 @@ int AggiungiGrafo(FILE *fin, int d){
             matrice[i][j] = 0;
         }
     }
-    int colonna=0;
-    int caratteriLetti;
+    unsigned int colonna=0;
+    unsigned int caratteriLetti;
     char riga[BUFFER];
     for (int i = 0; i < d; i++)
     {
-        if(fgets(riga, BUFFER, fin) == NULL) printf("Problema 1\n");
+        if(fgets(riga, BUFFER, stdin) == NULL) printf("Problema 1\n");
         caratteriLetti = strlen(riga);
         if(riga[caratteriLetti-1] == '\n') riga[caratteriLetti-1]='\0';
         for(int j=0;j<caratteriLetti;j++){
@@ -361,5 +336,5 @@ int AggiungiGrafo(FILE *fin, int d){
         }
         colonna = 0;
     }
-    return Dijkstra(d,matrice);    
+    return Dijkstra(d,matrice,Q,u);    
 }
